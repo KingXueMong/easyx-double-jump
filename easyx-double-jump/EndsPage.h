d6ea65006e724b3f187e0dbb1d61f60ec90418e2
1531323314 <44350821+1531323314@users.noreply.github.com>
2018-11-30 18:22:56 +0800

    11.30_18:22

--- easyx-double-jump/EndsPage.h
+++ easyx-double-jump/EndsPage.h
@@ -1,15 +1,16 @@
 #pragma once
 #include"GlobalVar.h"
 #include<stdio.h>
+
 #define WenJianName 
 void EndsPage() {
 	// 获取默认绘图窗口的 HDC 句柄
 	HDC hdc = GetImageHDC();
-	IMAGE img(WIDTH, HEIGHT);
+	IMAGE img(WIDTH, HEIGHT); 
 	IMAGE img_temp;
-	const TCHAR mulu[6][60] = { L"C:\\Users\\kingxue\\Desktop\\游戏\\sucai\\1.png" ,L"C:\\Users\\kingxue\\Desktop\\游戏\\sucai\\2.png",L"C:\\Users\\kingxue\\Desktop\\游戏\\sucai\\3.png",L"C:\\Users\\kingxue\\Desktop\\游戏\\sucai\\4.png",L"C:\\Users\\kingxue\\Desktop\\游戏\\sucai\\5.png",L"C:\\Users\\kingxue\\Desktop\\游戏\\sucai\\6.png" };
+	const TCHAR mulu[6][60] = { L"EndsPage1.png" ,L"EndsPage2.png",L"EndsPage3.png",L"EndsPage4.png",L"EndsPage5.png",L"EndsPage6.png" };
 	// 读取图片至绘图窗口
-	loadimage(&img, _T("C:\\Users\\kingxue\\Desktop\\游戏\\sucai\\endsbeijing.jpg"));
+	loadimage(&img,_T("endsbeijing.jpg"));
 	putimage(0, 0, &img);
 	FlushBatchDraw();
 	Sleep(1000);
@@ -17,7 +18,7 @@ void EndsPage() {
 	{
 		getimage(&img_temp, 0, 0,0,0 );
 		loadimage(&img_temp, mulu[i]);
-		putimage(70, i *70, &img_temp);
+		putimage(70, i *80+50, &img_temp);
 		FlushBatchDraw();
 		Sleep(3000);
 	}
